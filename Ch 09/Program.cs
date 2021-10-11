b97d14be424a91ef05d12798d984e8afdbe78fd3
Jaehyeok <one01h@gmail.com>
2021-10-12 08:37:29 +0900

    Add files

---
+++ Ch 09/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EnScript
+{
+    public class CalcClass
+    {
+        public static long Add(long i, long j)
+        {
+            return (i+j);
+        }
+        public static long Mul(long i, long j)
+        {
+            return (i * j);
+        }
+        public static long Mod(long i, long j)
+        {
+            return (i % j);
+        }
+    }
+}
